c0b7b9c6bf1965bb1b66b02cfa3f523ad1f93d78
danik <me@subsonic.cz>
2016-04-10 00:03:34 +0200

    added Form.getElements() method

--- src/js/Nittro/Forms/Form.js
+++ src/js/Nittro/Forms/Form.js
@@ -26,6 +26,11 @@ _context.invoke('Nittro.Forms', function (DOM, Arrays, DateTime, FormData, Vendo
 
         },
 
+        getElements: function () {
+            return this._.form.elements;
+            
+        },
+
         setSubmittedBy: function (value) {
             this._.submittedBy = value;
             return this;
